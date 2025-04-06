353b4c782a8ae3fa68026bc0f8aa6d2f1edd7a89
Cowship580 <38453910+LeQuangNhan-BRVT@users.noreply.github.com>
2025-04-06 19:26:13 +0700

    Update api.js

--- Cilent/src/services/api.js
+++ Cilent/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from '../config/config';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://diemdanhserver.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
